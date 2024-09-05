208b4a53365ac9d51d6527d0719f576d52c82ec2
Dennis <dennislzf@gmail.com>
2024-09-05 11:59:23 -0700

    added migrate to next build

--- stripeSetup.ts
+++ stripeSetup.ts
@@ -1,7 +1,6 @@
 const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-console.log(process.env.STRIPE_SECRET_KEY)
 
 const currencyType = 'usd'
 const plans = [
