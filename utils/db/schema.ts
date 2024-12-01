635bfd0eeb379435a596af5f9e91f0a372ece066
reubence <reuben.rapose@gmail.com>
2024-12-01 11:30:56 +0530

    Refactor signup process to include user's name in the signup form - fixes auth related issues

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,14 +1,12 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users_table', {
-    id: serial('id').primaryKey(),
+    id: text('id').primaryKey(),
     name: text('name').notNull(),
     email: text('email').notNull().unique(),
     plan: text('plan').notNull(),
     stripe_id: text('stripe_id').notNull(),
 });
-
-
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
